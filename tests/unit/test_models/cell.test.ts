80c9c43363df87ec9ef5b91e843b7ace903463ad
Raghvendra Acharya <raghvendra.acharya95@gmail.com>
2021-12-28 23:28:41 +0530

    remove assert

--- tests/unit/test_models/cell.test.ts
+++ tests/unit/test_models/cell.test.ts
@@ -6,19 +6,16 @@ describe('Cell Class Tests', () => {
     describe('getXCoordinate', () => {
         it('should return the x-axis index of the cell', () => {
             expect(cell.getXCoordinate()).toEqual(0);
-            // assert.equal(cell.getXCoordinate, 0);
         });
     });
     describe('getColumnIndex', () => {
         it('should return y-axis index of the cell', () => {
             expect(cell.getYCoordinate()).toEqual(1);
-            // assert.equal(cell.getYCoordinate, 1);
         });
     });
     describe('getColor', () => {
         it('should return the color value of the cell', () => {
             expect(cell.getColor()).toEqual(0);
-            // assert.equal(cell.getColor,0)
         });
     });
 });
