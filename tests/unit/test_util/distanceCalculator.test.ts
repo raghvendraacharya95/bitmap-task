c1c4ac0a1404f87d5a70354c0c982442e4240b22
Raghvendra Acharya <raghvendra.acharya95@gmail.com>
2021-12-28 22:44:00 +0530

    fix typo

--- tests/unit/test_util/distanceCalculator.test.ts
+++ tests/unit/test_util/distanceCalculator.test.ts
@@ -10,12 +10,12 @@ describe('DistanceCalculator Class Tests', () => {
     const whiteCell: Cell = new Cell(1, 1, 1);
     describe('calculateDistanceToTheNearestWhite', () => {
         it('should return the distance of closest white cell from a given cell ', () => {
-            // expect(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, blackCell)).toEqual(2);
-            assert.equals(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, blackCell), 2);
+            expect(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, blackCell)).toEqual(2);
+            // assert.equals(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, blackCell), 2);
         });
         it('should return 0 if the given cell is already white', () => {
-            // expect(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitmap, whiteCell)).toEqual(0);
-            assert.equals(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, whiteCell),0);
+            expect(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, whiteCell)).toEqual(0);
+            // assert.equals(DistanceCalculator.calculateDistanceToTheClosetWhiteCell(bitMatrix, whiteCell),0);
         });
     });
 });
